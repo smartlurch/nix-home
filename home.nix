2a51d1c9df14874743d605fd95c449c511a12bcb
Eddie LeBlanc <eddie.leblanc@gmail.com>
2024-03-24 10:33:04 -0500

    add resurrect and continuum to tmux config

--- home.nix
+++ home.nix
@@ -153,7 +153,23 @@
           set -g @dracula-fixed-location "Metairie, La"
 			  '';
 		  }
-	  ];  
+	    {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          '';
+      }
+      {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+          '';
+      }
+    ];  
   };
 
   programs.git = {
